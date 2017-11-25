d38c8c51a5bf7f114d71eb0d0d6f7446c1b44ceb
Julien Micheletti <j.micheletti@orange.fr>
2017-11-25 10:04:44 +0100

    Première version du site + apprentissage symfony

---
+++ Symfony/src/PT/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,53 @@
+<?php
+
+// src/OC/PlatformBundle/Controller/AdvertController.php
+
+namespace PT\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class AdvertController extends Controller
+{
+    public function indexAction($page)
+    {
+        // On ne sait pas combien de pages il y a
+        // Mais on sait qu'une page doit être supérieure ou égale à 1
+        if ($page < 1) {
+            // On déclenche une exception NotFoundHttpException, cela va afficher
+            // une page d'erreur 404 (qu'on pourra personnaliser plus tard d'ailleurs)
+            throw new NotFoundHttpException('Page "' . $page . '" inexistante.');
+        }
+        return $this->render('PTPlatformBundle:Advert:index.html.twig', array('listAdverts' => array()));
+    }
+
+public function jouerAction()
+    {
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony2',
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
+        );
+
+        return $this->render('PTPlatformBundle:Advert:jouer.html.twig', array('advert' => $advert));
+    }
+
+    public function menuAction($limit)
+    {
+        // On fixe en dur une liste ici, bien entendu par la suite
+        // on la récupérera depuis la BDD !
+        $listAdverts = array(
+            array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+            array('id' => 5, 'title' => 'Mission de webmaster'),
+            array('id' => 9, 'title' => 'Offre de stage webdesigner')
+        );
+
+        return $this->render('PTPlatformBundle:Advert:menu.html.twig', array(
+            // Tout l'intérêt est ici : le contrôleur passe
+            // les variables nécessaires au template !
+            'listAdverts' => $listAdverts
+        ));
+    }
+}
---
+++ Symfony/src/PT/PlatformBundle/PTPlatformBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PT\PlatformBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PTPlatformBundle extends Bundle
+{
+}
