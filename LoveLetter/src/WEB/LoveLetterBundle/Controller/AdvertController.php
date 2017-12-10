385c1f42ee0f7dd8794b5e0f623f0fc4c63b0f56
Noah Willaime <noah.willaime9@etu.univ-lorraine.fr>
2017-12-10 17:52:08 +0100

    Merge

--- LoveLetter/src/WEB/LoveLetterBundle/Controller/AdvertController.php
+++ LoveLetter/src/WEB/LoveLetterBundle/Controller/AdvertController.php
@@ -183,7 +183,7 @@ class AdvertController extends Controller
                     return $response->setData(array("finP" => true, "gagnant" => $enemy->getUsername()));
                 }
                 return $this->redirectToRoute('oc_platform_gestion', array('nb_joueurs' => 2, 'finManche' => 2));
-            } else if ($manche->getTour() == $this->getUser()->getUsername()){
+            } else if ($manche->getTour() == $this->getUser()->getUsername() || $enemy_check != 0){
                 $check = 1;
                 $nb = rand(1, 16);
                 if ($pioche->getNbElements() != 0) {
